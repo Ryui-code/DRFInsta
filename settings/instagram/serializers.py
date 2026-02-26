c808edf76cd34852a878a3e9146c137a572172b9
Ryui-code <ulanbektalasbaev2020@gmail.com>
2026-02-26 20:56:01 +0600

    Hi@

---
+++ settings/instagram/serializers.py
@@ -0,0 +1,86 @@
+from django.contrib.auth import authenticate
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from .models import *
+import os
+from django.conf import settings
+import joblib
+
+model_path = os.path.join(settings.BASE_DIR, 'naive_model.pkl')
+joblib.load(model_path)
+
+vector_path = os.path.join(settings.BASE_DIR, 'vector.pkl')
+joblib.load(vector_path)
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=6)
+
+    class Meta:
+        model = UserProfile
+        fields = ('username', 'password', 'avatar', 'bio', 'hashtag', 'link', 'status', 'registered_date')
+
+    def create(self, validated_data):
+        user = UserProfile.objects.create_user(**validated_data)
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        username = attrs.get("username")
+        password = attrs.get("password")
+        user = authenticate(username=username, password=password)
+        if not user:
+            raise ValidationError("Invalid credentials.")
+        attrs["user"] = user
+        return attrs
+
+class FollowSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Follow
+        fields = "__all__"
+
+    def validate(self, attrs):
+        if attrs['follower'] == attrs['following']:
+            raise serializers.ValidationError("U can not subscribe to yourself.")
+        return attrs
+
+class PostSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
+
+    class Meta:
+        model = Post
+        fields = "__all__"
+
+class PostContentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostContent
+        fields = "__all__"
+
+class PostLikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostLike
+        fields = "__all__"
+
+    def validate(self, attrs):
+        if PostLike.objects.filter(post=attrs['post'], user=attrs['user']).exists():
+            raise serializers.ValidationError("U are already liked it.")
+        return attrs
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
+
+    class Meta:
+        model = Comment
+        fields = "__all__"
+
+class CommentLikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CommentLike
+        fields = "__all__"
+
+    def validate(self, attrs):
+        if CommentLike.objects.filter(comment=attrs['comment'], user=attrs['user']).exists():
+            raise serializers.ValidationError("Like is already exists.")
+        return attrs
---
+++ settings/instagram/views.py
@@ -0,0 +1,111 @@
+from rest_framework import viewsets, generics, permissions, status
+from rest_framework.generics import GenericAPIView
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from .models import *
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter, SearchFilter
+from .filter import UserFilterSet, PostFilterSet
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import (
+FollowSerializer,
+RegisterSerializer,
+LoginSerializer,
+PostSerializer,
+PostContentSerializer,
+PostLikeSerializer,
+CommentSerializer,
+CommentLikeSerializer,
+)
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+class LoginView(GenericAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        })
+
+class LogoutView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+        except Exception:
+            return Response({"detail": "Invalid token."}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"detail": "Successfully logged out."}, status=status.HTTP_205_RESET_CONTENT)
+
+class ProfileViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    search_fields = ['username']
+    filterset_class = UserFilterSet
+
+    def get_queryset(self):
+        return UserProfile.objects.filter(id=self.request.user.id)
+
+class FollowViewSet(viewsets.ModelViewSet):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(follower=self.request.user)
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    search_fields = ['user', 'hashtag']
+    filterset_class = PostFilterSet
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class PostContentViewSet(viewsets.ModelViewSet):
+    queryset = PostContent.objects.all()
+    serializer_class = PostContentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class PostLikeViewSet(viewsets.ModelViewSet):
+    queryset = PostLike.objects.all()
+    serializer_class = PostLikeSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class CommentLikeViewSet(viewsets.ModelViewSet):
+    queryset = CommentLike.objects.all()
+    serializer_class = CommentLikeSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
